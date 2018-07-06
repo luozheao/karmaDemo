46c7f58181292c0d73c9efba498d0c7a388307ee
luozheaoPlus <815149054@qq.com>
2018-07-06 09:26:07 +0800

    test

---
+++ test/hello.spec.js
@@ -0,0 +1,13 @@
+// describe('hello, unit test.', function(){
+//     it('should also be able to test', function(){
+//         expect(true).toBe(true);
+//     });
+//
+//     it('should be failed', function(){
+//         expect(true).toBe(false);
+//     })
+//
+//     it('hello world !',function(){
+//         expect("true haha").toBe('false hehe');
+//     })
+// });
